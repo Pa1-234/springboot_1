3fd2ce883f4e19e66b7010f1ebc81549e0c63691
Pa1-234 <150997748+Pa1-234@users.noreply.github.com>
2024-01-10 14:00:11 +0530

    Update MyMysqlProjectApplicationTests.java

--- myMysqlProject/src/test/java/com/mydomain/MyMysqlProjectApplicationTests.java
+++ myMysqlProject/src/test/java/com/mydomain/MyMysqlProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mydomain;
+//package com.mydomain;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MyMysqlProjectApplicationTests {
+//@SpringBootTest
+//class MyMysqlProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
